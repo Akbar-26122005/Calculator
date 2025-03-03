26ecd67fbd01a43c0a26a602f14e4f9fd14cf7e9
OLEG <hjdj123.mail.ru>
2025-03-03 13:01:27 +0500

    Реализованы функции сложения и вычитания

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+	static int Sum(int a, int b) = > a + b;
+	static int Subtraction(int a, int b) = > a - b;
+};
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	return 0;
+}
