a5130d95a2516b9b1ed5e5ba124f31b3d50dd0ac
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-03-03 13:12:38 +0500

    Реализована функция умножения

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ class Calculator {
 public:
 	static int Sum(int a, int b) = > a + b;
 	static int Subtraction(int a, int b) = > a - b;
+	static int Multiplication(int a, int b) = > a * b;
 };
 
 int main() {
