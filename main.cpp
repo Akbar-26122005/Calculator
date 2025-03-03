ebfa19c8464a59eec17f73119ab780c13c464ef5
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-03-03 13:15:00 +0500

    Реализована функция деления

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ public:
 	static int Sum(int a, int b) = > a + b;
 	static int Subtraction(int a, int b) = > a - b;
 	static int Multiplication(int a, int b) = > a * b;
+	static int Division(int a, int b) = > a / b;
 };
 
 int main() {
